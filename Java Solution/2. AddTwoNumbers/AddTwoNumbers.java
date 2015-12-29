779f659008f4beb0f61a9572de1087ea3fef6eb1
changan.luo <changan.luo@ele.me>
2015-12-29 22:46:19 +0800

    add AddTwoNumbers

---
+++ Java Solution/2. AddTwoNumbers/AddTwoNumbers.java
@@ -0,0 +1,21 @@
+package AddTwoNumbers;
+/**
+ * Definition for singly-linked list.
+ * public class Util.ListNode {
+ *     int val;
+ *     Util.ListNode next;
+ *     Util.ListNode(int x) { val = x; }
+ * }
+ */
+
+
+import Util.ListNode;
+
+/**
+ * Created by Wayne on 15/12/29.
+ */
+public class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return null;
+    }
+}
---
+++ Java Solution/Util/ListNode.java
@@ -0,0 +1,10 @@
+package Util;
+
+/**
+ * Created by Wayne on 15/12/29.
+ */
+public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x){val = x;}
+}
