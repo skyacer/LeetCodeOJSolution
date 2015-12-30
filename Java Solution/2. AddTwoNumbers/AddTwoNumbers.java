4569b9dea67d3ef933c3a4ace219730bb7e56b37
changan.luo <changan.luo@ele.me>
2015-12-30 23:45:32 +0800

    add AddTwoNumbers

--- Java Solution/2. AddTwoNumbers/AddTwoNumbers.java
+++ Java Solution/2. AddTwoNumbers/AddTwoNumbers.java
@@ -18,4 +18,51 @@ public class AddTwoNumbers {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         return null;
     }
+
+    public static ListNode reverseList(ListNode head){
+        if(head == null || head.next==null){
+            return head;
+        }
+
+        ListNode cur = head;
+        ListNode reverse = null;
+
+        while(cur!= null){
+            ListNode next = cur.next;
+            cur.next = reverse;
+            reverse = cur;
+            cur = next;
+        }
+        return reverse;
+    }
+
+    public static void main(String[] args){
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(3);
+
+        a.next = b;
+        b.next = c;
+
+        ListNode cur = a;
+
+        while (cur!=null){
+            System.out.println(cur.val);
+            cur = cur.next;
+        }
+
+        ListNode reverse = reverseList(a);
+
+
+        ListNode reverseCur = reverse;
+
+        while(reverseCur!= null){
+            System.out.println(reverseCur.val);
+            reverseCur = reverseCur.next;
+        }
+
+
+    }
+
+
 }
