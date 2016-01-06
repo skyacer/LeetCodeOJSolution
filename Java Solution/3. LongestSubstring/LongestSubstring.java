f469c3466f6db8d3e9d368574df8bd3bf04813d0
changan.luo <changan.luo@ele.me>
2016-01-06 23:31:14 +0800

    add Longest Substring

---
+++ Java Solution/3. LongestSubstring/LongestSubstring.java
@@ -0,0 +1,10 @@
+package LongestSubstring;
+
+/**
+ * Created by Wayne on 16/1/6.
+ */
+public class LongestSubstring {
+    public int lengthOfLongestSubstring(String s) {
+        return 0;
+    }
+}
