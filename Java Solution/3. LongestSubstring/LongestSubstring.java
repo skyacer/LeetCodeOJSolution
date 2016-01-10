2d99ce015d7639ac21dbbba789f8145669fe7ca9
skyacer <554298448@qq.com>
2016-01-10 23:42:18 +0800

    add

--- Java Solution/3. LongestSubstring/LongestSubstring.java
+++ Java Solution/3. LongestSubstring/LongestSubstring.java
@@ -4,7 +4,7 @@ package LongestSubstring;
  * Created by Administrator on 2016/1/9 0009.
  */
 public class LongestSubstring {
-    public int lengthOfLongestSubstring(String s) {
+    public static int lengthOfLongestSubstring(String s) {
         if(s.length()<2){
             return s.length();
         }
@@ -12,8 +12,23 @@ public class LongestSubstring {
         char[] stringArray = s.toCharArray();
 
         int max=0,count=0,tail=0;
+        for(int i = 0;i<counters.length;i++){
+            if(counters[stringArray[i]]==0){
+                counters[stringArray[i]]++;
+                if(++count>max){
+                    max = count;
+                }
+            }else{
 
+            }
+        }
         return max;
+    }
+    
+    public static void main(String[] args){
+        String s = "abcdabcdddd";
+        int out = lengthOfLongestSubstring(s);
+        System.out.println(out);
     }
 
 }
