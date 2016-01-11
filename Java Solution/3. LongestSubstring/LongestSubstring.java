efc845d8b11d82bd6d1edbe4544c01671bafa7f9
skyacer <554298448@qq.com>
2016-01-11 23:41:55 +0800

    add longestsubstring

--- Java Solution/3. LongestSubstring/LongestSubstring.java
+++ Java Solution/3. LongestSubstring/LongestSubstring.java
@@ -12,21 +12,26 @@ public class LongestSubstring {
         char[] stringArray = s.toCharArray();
 
         int max=0,count=0,tail=0;
-        for(int i = 0;i<counters.length;i++){
+        for(int i = 0;i<stringArray.length;i++){
             if(counters[stringArray[i]]==0){
                 counters[stringArray[i]]++;
                 if(++count>max){
                     max = count;
                 }
-            }else{
 
+            }else{
+                while(stringArray[i]!=stringArray[tail]&&tail<i){
+                    count--;
+                    counters[stringArray[tail++]]--;
+                }
+                tail++;
             }
         }
         return max;
     }
-    
+
     public static void main(String[] args){
-        String s = "abcdabcdddd";
+        String s = "pwwkew";
         int out = lengthOfLongestSubstring(s);
         System.out.println(out);
     }
